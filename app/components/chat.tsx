5420c013de8ca21b4d1726d6bde824a9c9cbbb07
Shubhanjan Medhi <s.medhi1@gmail.com>
2025-06-14 20:43:35 +0530

    test

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -34,7 +34,7 @@ const ChatComponent: React.FC = () => {
         console.log(isLoading);
         setMessages(prev => [...prev, { role: 'loading' }]);
         try {
-            const res = await fetch(`https://pdf-chat-server.onrender.com/chat?message=${message}`);
+            const res = await fetch(`https://0fa7-2401-4900-1cba-6ede-8141-901b-b904-e9f0.ngrok-free.app/chat?message=${message}`);
             const data = await res.json()
             setMessages(prev => [...prev.filter(msg => msg.role !== 'loading'), { 
                 role: 'assistant', 
